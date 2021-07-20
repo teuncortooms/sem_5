9563ac5b0f69f969e3ceabcde3610701a9561933
Teun Cortooms <teuncortooms@gmail.com>
2021-07-20 16:25:32 +0200

    api url in angular variables + disable ssl in api launch settings

--- AspnetCoreWithAngular/AngularUI/src/environments/environment.prod.ts
+++ AspnetCoreWithAngular/AngularUI/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
   production: true,
-  weatherEndpoint: 'https://localhost:60646/weather'
 };
--- AspnetCoreWithAngular/AngularUI/src/environments/environment.ts
+++ AspnetCoreWithAngular/AngularUI/src/environments/environment.ts
@@ -2,9 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const myAPI = "http://localhost:35446/";
+
 export const environment = {
   production: false,
-  weatherEndpoint: 'https://localhost:60646/weather'
+  weatherEndpoint: myAPI + "weatherforecast"
 };
 
 /*
